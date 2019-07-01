c22ec3c0527ab5f8c8bf6c491d20cbb5fce7f87b
Jean Lobo <jeanloboxd@gmail.com>
2019-07-01 13:04:55 -0300

    chore: Criado rotas para Listar Projetos e Update do Projeto

--- index.js
+++ index.js
@@ -2,4 +2,24 @@ const express = require("express");
 
 const server = express();
 
+server.use(express.json());
+
+const projects = ["MeuPet", "Pomodor", "AppContabil"];
+
+//Read all projects
+server.get("/projects", (req, res) => {
+  return res.json(projects);
+});
+
+//Update one project
+server.put("/projects/:id", (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id === id);
+  project.title = title;
+
+  return res.json(project);
+});
+
 server.listen(3000);
